ec676c85f700d86b97cf363318d76350458829bb
Sahil Abbas <sa9300432@gmail.com>
2026-01-16 23:08:59 +0530

    Switch nodemailer to resend and fixed loader/response

--- controllers/getorders.js
+++ controllers/getorders.js
@@ -1,23 +1,21 @@
 import orders from "../modles/orderschema.js"
-import transpoter from "../utils/Emailsend.js";
+import sendEmail from "../utils/Emailsend.js";
 
 export const Getorders = async (req,res) =>{
 
 const {name,email,date,packages,massage} = req.body;
 
-const Orders =await orders.create({name,email,date,packages,massage})
+const Orders = await orders.create({name,email,date,packages,massage})
 
-await transpoter.sendMail({
-    from: process.env.EMAIL,
-    to: process.env.EMAIL,
-
-    subject: "New order Received",
-    html:`<h2> New order<h2/>
-          <p><b>Name:<b/>${name}<p/>
-           <p><b>email:<b/>${email}<p/>
-            <p><b>date:<b/>${date}<p/>
-             <p><b>packages:<b/>${packages}<p/>
-              <p><b>massages:<b/>${massage}<p/> `
-});
-    res.json({massage:"Order sucessfully.."})
+if (process.env.EMAIL && process.env.EMAIL_PASS) {
+   try {
+       await sendEmail(orders)
+   } catch (error) {
+    console.log("email faild but order save")
+   } 
+}
+    res.status(201).json({
+        sucess:true,
+        massage:"Order sucessfully.."}
+    )
 }
--- utils/Emailsend.js
+++ utils/Emailsend.js
@@ -1,14 +1,30 @@
 import dotenv from "dotenv"
 dotenv.config();
+import { Resend } from "resend"
 
-import nodemailer from "nodemailer";
+//import nodemailer from "nodemailer";
+//const transpoter = nodemailer.createTransport({
+//service: "gmail",
+   // auth: {
+    //user: process.env.EMAIL,
+      //  pass: process.env.EMAIL_PASS,
+   // },
+//});
 
-const transpoter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS,
-    },
- });
 
- export default transpoter;
+const resend = new Resend(process.env.RESEND_API_KEY);
+export const sendEmail = async (orders) => {
+    await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: `${EMAIL}`,
+        subject: "New order Received",
+        html: `<h2> New order<h2/>
+         <p><b>Name:<b/>${orders.name}<p/>
+        <p><b>email:<b/>${orders.email}<p/>
+         <p><b>date:<b/>${orders.date}<p/>
+         <p><b>packages:<b/>${orders.packages}<p/>
+         <p><b>massages:<b/>${orders.massage}<p/> `,
+    })
+}
+
+export default sendEmail;
