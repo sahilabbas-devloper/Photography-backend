04c809e23f68c9ad8143bd8b4c74f1877511b49a
Sahil Abbas <sa9300432@gmail.com>
2026-04-20 22:53:04 +0530

    cors middelware updaetd'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import ordersRoutes from "./routes/orderRoutes.js"
 const app = express();
 app.use(cors({
     methods:["POST","GET"],
-    origin:["http://localhost:5173/","photography-web-un.vercel.app"]
+    origin:["http://localhost:5173/","https://photography-web-un.vercel.app"]
 }))
 app.use(express.json())
 
